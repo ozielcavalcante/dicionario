98313b6b4e562fb31db513137c79ae3462d7b1c0
Oziel Alves Cavalcante <oziel@tjro.jus.br>
2023-08-18 07:49:16 -0400

    busca dicionario sem acento e filtra baseado em input

---
+++ autocomplete.js
@@ -0,0 +1,38 @@
+fetch("https://corsproxy.io/?https://www.ime.usp.br/~pf/dicios/br-sem-acentos.txt")
+.then(res => res.text())
+.then(palavras => filtrar(palavras.split('\n')))
+
+const autocompleteInput = document.getElementById('autocomplete');
+
+function filtrar(palavras) {
+  autocompleteInput.oninput = () => {
+    const inputText = autocompleteInput.value.toLowerCase();
+    const filtrados = palavras.filter(p => p.toLowerCase().startsWith(inputText));
+    listarFiltrados(filtrados);
+  };
+}
+
+function listarFiltrados(opcoes) {
+  const dropdown = document.getElementById('autocomplete-dropdown');
+  if (dropdown) {
+    dropdown.remove();
+  }
+
+  if (opcoes.length > 0) {
+    const dropdownList = document.createElement('ul');
+    dropdownList.id = 'autocomplete-dropdown';
+
+    opcoes.forEach(option => {
+      const listItem = document.createElement('li');
+      listItem.textContent = option;
+      listItem.addEventListener('click', function() {
+        autocompleteInput.value = option;
+        dropdownList.remove();
+      });
+
+      dropdownList.appendChild(listItem);
+    });
+
+    autocompleteInput.parentNode.appendChild(dropdownList);
+  }
+}
